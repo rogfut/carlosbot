126339e63e19bda3390a93e98957c2ef3de5238a
rogfut <rogfut@outlook.com>
2019-06-28 00:38:04 -0700

    Add XRP-USD pair, emoji support

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@
 
 const { ActivityHandler } = require('botbuilder');
 const axios = require('axios');
+const emoji = require('node-emoji');
+
+// emojis for later use
+const dollarEmoji = emoji.get('dollar');
 
 class MyBot extends ActivityHandler {
     constructor() {
@@ -15,20 +19,24 @@ class MyBot extends ActivityHandler {
                 await context.sendActivity('https://gfycat.com/agiletastyindianjackal');
             }
             if (context.activity.text.toLowerCase().includes('hi carlosbot') || context.activity.text.toLowerCase().includes('hi @carlosbot')) {
-                await context.sendActivity('Wassa wassa wassa wassa wassa wassa WASSUP BITCONNECT');
+                await context.sendActivity(emoji.get('wave') + ' Wassa wassa wassa wassa wassa wassa WASSUP BITCONNECT');
             }
             switch (context.activity.text) {
             case '/p btc':
                 let btcPrice = await axios.get('https://api.coinbase.com/v2/prices/BTC-USD/spot?currency=USD');
-                await context.sendActivity('BTC $' + btcPrice.data.data.amount);
+                await context.sendActivity('BTC $' + btcPrice.data.data.amount + ' ' + dollarEmoji);
                 break;
             case '/p ltc':
                 let ltcPrice = await axios.get('https://api.coinbase.com/v2/prices/LTC-USD/spot?currency=USD');
-                await context.sendActivity('LTC $' + ltcPrice.data.data.amount);
+                await context.sendActivity('LTC $' + ltcPrice.data.data.amount + ' ' + dollarEmoji);
                 break;
             case '/p eth':
                 let ethPrice = await axios.get('https://api.coinbase.com/v2/prices/ETH-USD/spot?currency=USD');
-                await context.sendActivity('ETH $' + ethPrice.data.data.amount);
+                await context.sendActivity('ETH $' + ethPrice.data.data.amount + ' ' + dollarEmoji);
+                break;
+            case '/p xrp':
+                let xrpPrice = await axios.get('https://api.coinbase.com/v2/prices/XRP-USD/spot?currency=USD');
+                await context.sendActivity('XRP $' + xrpPrice.data.data.amount + ' ' + dollarEmoji);
                 break;
             case '/bitconnect':
                 await context.sendActivity('https://youtu.be/e5nyQmaq4k4');
